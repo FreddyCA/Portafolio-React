bbe451ed226b7ea37a81edc18d6c8f2f5dc018f3
Freddy Coaricona <fcoaricona@outlook.com>
2023-10-23 23:56:16 -0400

    academic logos, datos

--- src/components/AcademicButton/AcademicButton.jsx
+++ src/components/AcademicButton/AcademicButton.jsx
@@ -1,45 +1,117 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import LogoUmsa from "../../assets/logos/umsalogo.png";
+import LogoOne from "../../assets/logos/ONElogo.png";
+import LogoAlura from "../../assets/logos/logo-aluraespanhol.svg";
+import LogoOP from "../../assets/logos/OPlogo.png";
+import LogoLaunch from "../../assets/logos/logoLaunchX.png";
 
 const AcademicButtonContentStyle = styled.div`
   width: 100%;
-  /* height: 250px; */
-  background-color: orange;
+  /* height: 300px; */
   display: flex;
-  justify-content: space-between;
-  gap: 20px;
+  justify-content: space-evenly;
+  gap: 2rem;
 `;
 
 const AcademicButtonItemStyle = styled.div`
-  width: 200px;
-  height: 250px;
-  background-color: red;
+  min-width: 200px;
+  height: 300px;
+  border: 2px solid rgba(100, 275, 0, 0.5);
+  background-color: rgba(250, 200, 275, 0.7);
+  border-end-end-radius: 10px ;
+  border-end-start-radius: 10px ;
+
+  cursor: pointer;
 `;
 
-const AcademicImgStyle = styled.div`
-  height: 60%;
-  background-color: blanchedalmond;
+const AcademicImgContentStyle = styled.div`
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const AcademicImgStyle = styled.img`
+  height: auto;
+`;
+const AcademicImgMediumStyle = styled.div`
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90px;
+`;
+
+const AcademicTextContent = styled.div`
+  background-color: slategray;
+
+  height: 30%;
+  display: flex;
+  padding: 1rem 0 0;
+  gap: 0.5rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-end-end-radius: 10px ;
+  border-end-start-radius: 10px ;
 `;
 
 const AcademicText = styled.p`
-  background-color: yellow;
-  min-height: 20%;
-  padding: 0 0 1rem;
-  display: flex;
-  align-items: end;
-  justify-content: center;
+  width: 100%;
+  text-align: center;
+  color: var(--color--textoPrincipal);
+  font-weight: 700;
+
+  ${(props) =>
+    props.$bold &&
+    css`
+      font-weight: 300;
+      font-size: 0.85rem;
+    `}
 `;
 
 const AcademicButton = () => {
   return (
     <AcademicButtonContentStyle>
       <AcademicButtonItemStyle>
-        <AcademicImgStyle></AcademicImgStyle>
-        <AcademicText>a</AcademicText>
-        <AcademicText>a</AcademicText>
+        <AcademicImgContentStyle>
+          <AcademicImgStyle src={LogoUmsa} alt="LOGO"></AcademicImgStyle>
+        </AcademicImgContentStyle>
+        <AcademicTextContent>
+          <AcademicText>Ingeniería Electrónica</AcademicText>
+          <AcademicText $bold>En Curso - UMSA</AcademicText>
+        </AcademicTextContent>
       </AcademicButtonItemStyle>
-      <AcademicButtonItemStyle></AcademicButtonItemStyle>
-      <AcademicButtonItemStyle></AcademicButtonItemStyle>
-      <AcademicButtonItemStyle></AcademicButtonItemStyle>
+
+      <AcademicButtonItemStyle>
+        <AcademicImgContentStyle>
+          <AcademicImgStyle src={LogoOne} alt="LOGO"></AcademicImgStyle>
+          <AcademicImgMediumStyle>
+            <AcademicImgStyle src={LogoAlura} alt="LOGO"></AcademicImgStyle>
+          </AcademicImgMediumStyle>
+        </AcademicImgContentStyle>
+        <AcademicTextContent>
+          <AcademicText>Ingeniería Electrónica</AcademicText>
+          <AcademicText $bold>En Curso - UMSA</AcademicText>
+        </AcademicTextContent>
+      </AcademicButtonItemStyle>
+      <AcademicButtonItemStyle>
+        <AcademicImgContentStyle>
+          <AcademicImgStyle src={LogoOP} alt="LOGO"></AcademicImgStyle>
+        </AcademicImgContentStyle>
+        <AcademicTextContent>
+          <AcademicText>Ingeniería Electrónica</AcademicText>
+          <AcademicText $bold>En Curso - UMSA</AcademicText>
+        </AcademicTextContent>
+      </AcademicButtonItemStyle>
+      <AcademicButtonItemStyle>
+        <AcademicImgContentStyle>
+          <AcademicImgStyle src={LogoLaunch} alt="LOGO"></AcademicImgStyle>
+        </AcademicImgContentStyle>
+        <AcademicTextContent>
+          <AcademicText>Ingeniería Electrónica</AcademicText>
+          <AcademicText $bold>En Curso - UMSA</AcademicText>
+        </AcademicTextContent>
+      </AcademicButtonItemStyle>
     </AcademicButtonContentStyle>
   );
 };
--- src/sections/Academic/Academic.jsx
+++ src/sections/Academic/Academic.jsx
@@ -1,27 +1,22 @@
 import styled from "styled-components";
-import ContentSections from "../../components/ContentSections/ContentSections";
 import Subtitle from "../../components/Subtitle/Subtitle";
 import AcademicButton from "../../components/AcademicButton/AcademicButton";
 
 const AcademicStyle = styled.div`
-  /* height: 200px; */
   width: 100%;
-  padding: 2rem 0;
+  padding: 2rem ;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   gap: 2rem;
 `;
 
 const Academic = () => {
   return (
-    <ContentSections>
-      <AcademicStyle>
-        <Subtitle bold>Formación Académica</Subtitle>
-        <AcademicButton></AcademicButton>
-      </AcademicStyle>
-    </ContentSections>
+    <AcademicStyle>
+      <Subtitle bold>Formación Académica</Subtitle>
+      <AcademicButton></AcademicButton>
+    </AcademicStyle>
   );
 };
 
