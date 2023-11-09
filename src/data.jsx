fe6a7a743b89d27581af4035b50140d29b64ed9c
Freddy Coaricona <fcoaricona@outlook.com>
2023-11-09 00:28:50 -0400

    use relative paths for images ../

--- src/data.jsx
+++ src/data.jsx
@@ -30,115 +30,115 @@ const data = {
   "hardSkills": [
     {
       "titulo": "HTML",
-      "img": "./src/assets/logos/html5.png",
+      "img": "../src/assets/logos/html5.png",
     },
     {
       "titulo": "CSS",
-      "img": "./src/assets/logos/ccs.png",
+      "img": "../src/assets/logos/ccs.png",
     },
     {
       "titulo": "JavaScript",
-      "img": "./src/assets/logos/js.png",
+      "img": "../src/assets/logos/js.png",
     },
     {
       "titulo": "React",
-      "img": "./src/assets/logos/react.png",
+      "img": "../src/assets/logos/react.png",
     },
     {
       "titulo": "TypeScript",
-      "img": "./src/assets/logos/typescript.png",
+      "img": "../src/assets/logos/typescript.png",
     },
     {
       "titulo": "Bootstrap",
-      "img": "./src/assets/logos/bootstrap.png",
+      "img": "../src/assets/logos/bootstrap.png",
     },
     {
       "titulo": "Sass",
-      "img": "./src/assets/logos/sass.png",
+      "img": "../src/assets/logos/sass.png",
     },
     {
       "titulo": "Material-UI",
-      "img": "./src/assets/logos/material-ui.png",
+      "img": "../src/assets/logos/material-ui.png",
     },
     {
       "titulo": "Node.js",
-      "img": "./src/assets/logos/node.png",
+      "img": "../src/assets/logos/node.png",
     },
     {
       "titulo": "Git",
-      "img": "./src/assets/logos/git.png",
+      "img": "../src/assets/logos/git.png",
     },
     {
       "titulo": "GitHub",
-      "img": "./src/assets/logos/github.png",
+      "img": "../src/assets/logos/github.png",
     },
     {
       "titulo": "OCI",
-      "img": "./src/assets/logos/oracle.png",
+      "img": "../src/assets/logos/oracle.png",
     },
     {
       "titulo": "MySQL",
-      "img": "./src/assets/logos/mysql.png",
+      "img": "../src/assets/logos/mysql.png",
     },
     {
       "titulo": "Firebase",
-      "img": "./src/assets/logos/firebase.png",
+      "img": "../src/assets/logos/firebase.png",
     },
     {
       "titulo": "Python",
-      "img": "./src/assets/logos/python.png",
+      "img": "../src/assets/logos/python.png",
     },
     {
       "titulo": "C++",
-      "img": "./src/assets/logos/c-plus-plus.png",
+      "img": "../src/assets/logos/c-plus-plus.png",
     },
   ],
   "softSkills": [
     {
       "titulo": "Creatividad",
-      "img": "./src/assets/logos/creatividad.png",
+      "img": "../src/assets/logos/creatividad.png",
     },
     {
       "titulo": "Superación",
-      "img": "./src/assets/logos/superation.png",
+      "img": "../src/assets/logos/superation.png",
     },
     {
       "titulo": "Planificación",
-      "img": "./src/assets/logos/planification.png",
+      "img": "../src/assets/logos/planification.png",
     },
     {
       "titulo": "Assertividad",
-      "img": "./src/assets/logos/assertividad.png",
+      "img": "../src/assets/logos/assertividad.png",
     },
     {
       "titulo": "Discernimiento",
-      "img": "./src/assets/logos/discernimiento.png",
+      "img": "../src/assets/logos/discernimiento.png",
     },
     {
       "titulo": "Colaboración",
-      "img": "./src/assets/logos/colaboration.png",
+      "img": "../src/assets/logos/colaboration.png",
     },
   ],
   "hobbies": [
     {
       "titulo": "Tocar trompeta",
-      "img": "./src/assets/logos/trompeta.png",
+      "img": "../src/assets/logos/trompeta.png",
     },
     {
       "titulo": "Lectura",
-      "img": "./src/assets/logos/lectura.png",
+      "img": "../src/assets/logos/lectura.png",
     },
     {
       "titulo": "Escuchar música",
-      "img": "./src/assets/logos/musica.png",
+      "img": "../src/assets/logos/musica.png",
     },
     {
       "titulo": "Fútbol",
-      "img": "./src/assets/logos/futbol.png",
+      "img": "../src/assets/logos/futbol.png",
     },
     {
       "titulo": "Electrónica",
-      "img": "./src/assets/logos/micro.png",
+      "img": "../src/assets/logos/micro.png",
     }
   ],
   "experience": [
@@ -147,49 +147,49 @@ const data = {
       "description": "Plataforma de Gestión de Productos en Línea.",
       "repository": "https://github.com/FreddyCA/AluraGeek-Store",
       "demo": "https://freddyca.github.io/AluraGeek-Store/",
-      "img": "./src/assets/exp/alurageek.png"
+      "img": "../src/assets/exp/alurageek.png"
     },
     {
       "title": "AluraFlix",
       "description": "Aplicación web con características similares a una plataforma de video.",
       "repository": "https://github.com/FreddyCA/AluraFlix",
       "demo": "https://github.com/FreddyCA/AluraFlix",
-      "img": "./src/assets/exp/aluraflix.png"
+      "img": "../src/assets/exp/aluraflix.png"
     },
     {
       "title": "Encriptador",
       "description": "Aplicación web de encriptación de texto en línea.",
       "repository": "https://github.com/FreddyCA/Encriptador-ONE-alura-LATAM",
       "demo": "https://freddyca.github.io/Encriptador-ONE-alura-LATAM/",
-      "img": "./src/assets/exp/encriptador.png"
+      "img": "../src/assets/exp/encriptador.png"
     },
     {
       "title": "Apeperia",
       "description": "Landing page enfocada en el desarrollo y mantenimiento de aplicaciones web, creada a través del programa ONE.",
       "repository": "https://github.com/FreddyCA/Home-Apeperia",
       "demo": "https://freddyca.github.io/Home-Apeperia/",
-      "img": "./src/assets/exp/homeApeperia.png"
+      "img": "../src/assets/exp/homeApeperia.png"
     },
     {
       "title": "Coinbase",
       "description": "Landing page que explora y explica el mundo de las criptomonedas, desarrollada como parte del programa Open Bootcamp.",
       "repository": "https://github.com/FreddyCA/Coinbase",
       "demo": "https://freddyca.github.io/Coinbase/",
-      "img": "./src/assets/exp/coinbase.png"
+      "img": "../src/assets/exp/coinbase.png"
     },
     {
       "title": "Fruta & Fruto",
       "description": "Página web enfocada en recetas, desarrollada a través del programa ONE.",
       "repository": "https://github.com/FreddyCA/Fruta-Y-Fruto",
       "demo": "https://freddyca.github.io/Fruta-Y-Fruto/",
-      "img": "./src/assets/exp/frutayfruto.png"
+      "img": "../src/assets/exp/frutayfruto.png"
     },
     {
       "title": "Mini Alura",
       "description": "Página web que representa una versión más concisa de un sitio web extenso desarrollado a través del programa ONE.",
       "repository": "https://github.com/FreddyCA/Mini-Alura",
       "demo": "https://freddyca.github.io/Mini-Alura/",
-      "img": "./src/assets/exp/miniAlura.png"
+      "img": "../src/assets/exp/miniAlura.png"
 
     },
     {
@@ -197,39 +197,39 @@ const data = {
       "description": "Prototipo para mejorar y completar un proyecto de comercio electrónico.",
       "repository": "https://github.com/FreddyCA/Carrito-de-Compras",
       "demo": "https://freddyca.github.io/Carrito-de-Compras/",
-      "img": "./src/assets/exp/carritodeCompras.png"
+      "img": "../src/assets/exp/carritodeCompras.png"
     },
     {
       "title": "ORG",
       "description": "Página web de administración de personal desarrollado a través del programa ONE.",
       "repository": "https://github.com/FreddyCA/ORG-React",
       "demo": "https://org-freddyca.vercel.app/",
-      "img": "./src/assets/exp/org.png"
+      "img": "../src/assets/exp/org.png"
     }
   ],
   "academic": [
     {
-      "img": "./src/assets/logos/umsalogo.png",
+      "img": "../src/assets/logos/umsalogo.png",
       "title": "Ingeniería Electrónica",
       "status": "En Curso - UMSA",
       "alt": "logo UMSA",
     },
     {
-      "img": "./src/assets/logos/ONElogo.png",
-      "img2": "./src/assets/logos/logo-aluraespanhol.svg",
+      "img": "../src/assets/logos/ONElogo.png",
+      "img2": "../src/assets/logos/logo-aluraespanhol.svg",
       "title": "Front End",
       "status": "2023 - Oracle, Alura",
       "alt": "logo One",
       "alt2": "logo Alura",
     },
     {
-      "img": "./src/assets/logos/OPlogo.png",
+      "img": "../src/assets/logos/OPlogo.png",
       "title": "Front End",
       "status": "2023 - Open Bootcamp",
       "alt": "logo Open Bootcamp",
     },
     {
-      "img": "./src/assets/logos/logoLaunchX.png",
+      "img": "../src/assets/logos/logoLaunchX.png",
       "title": "Full Stack Developer",
       "status": "2022 - Innovacción virtual",
       "alt": "logo Launch X",
